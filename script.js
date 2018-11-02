eba4658e2729842c728513486f8343618ba80017
hsjeevan <hsjeevan07@gmail.com>
2018-11-02 14:30:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function largest (a,b,c,d){
+var largest=a;
+if (b>largest) {
+	largest=b;}
+else if (c>largest) {
+	largest=c;}
+else if (d>largest) {
+	largest=d;
+}console.log(largest);
+}
+largest(-8,12.5,-6.769,0,-1);
